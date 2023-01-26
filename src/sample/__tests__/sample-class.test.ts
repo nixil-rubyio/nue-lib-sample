d64415ee65fb551950b6a1926eda75949de6426d
wenbo <wenbo.lyu@nue.io>
2023-01-26 13:54:38 -0800

    add self service dependency

--- src/sample/__tests__/sample-class.test.ts
+++ src/sample/__tests__/sample-class.test.ts
@@ -1,9 +1,16 @@
 import { SampleClass } from "@nue/ui-lib-sample";
+import { NuePricingChangeOrderCalculator } from "@nue-apps/nue-pricing-clientjs";
 
 describe("Test Sample class", () => {
   it("greet should work", () => {
     let sampleClass = new SampleClass("Alex");
     let greet = sampleClass.greet();
     expect(greet).toEqual("Hello, Alex");
+  });
+
+  it("it should preview change order", () => {
+    const calculator = new NuePricingChangeOrderCalculator({
+      allowChangeOrderBackDate: true,
+    });
   });
 });
